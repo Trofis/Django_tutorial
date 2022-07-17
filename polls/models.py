8b4406b14a548b83b6ce8999ee2fa74d7a4cd362
Trofis <porto.thomas@gmail.com>
2022-07-17 22:51:17 +0200

    feat(polls): add details, results & vote views + updating routing & index's view

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from django.utils import timezone
 
@@ -7,7 +9,7 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
-        return "{2} : {1}".format(self.question_text,self.pub_date)
+        return "{1} : {0}".format(self.question_text,self.pub_date)
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
@@ -18,4 +20,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return "Question {1} : {2} (votes {3})".format(self.question,self.choice_text, self.votes)
+        return "Question {0} : {1} (votes {2})".format(self.question,self.choice_text, self.votes)
